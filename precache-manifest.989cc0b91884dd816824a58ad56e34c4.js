d07ef6212c2c86963291d8b8336658b55b21bff4
arvindcyclist <yadav.arvind870@gmail.com>
2020-04-27 15:56:42 +0530

    Updates

---
+++ precache-manifest.989cc0b91884dd816824a58ad56e34c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2fe1b91529dbd0a894b0760996f5366",
+    "url": "/sportsworld/index.html"
+  },
+  {
+    "revision": "6275928201c1e7472a27",
+    "url": "/sportsworld/static/css/main.8e471a26.chunk.css"
+  },
+  {
+    "revision": "9cb31a84315398d1e9ac",
+    "url": "/sportsworld/static/js/2.4145c0f7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sportsworld/static/js/2.4145c0f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6275928201c1e7472a27",
+    "url": "/sportsworld/static/js/main.e90cd690.chunk.js"
+  },
+  {
+    "revision": "d827930240acbe5f593f",
+    "url": "/sportsworld/static/js/runtime-main.f231533a.js"
+  }
+]);
