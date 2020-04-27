1e4490ce3f9358371afcb637ddb042d36bf19129
arvindcyclist <yadav.arvind870@gmail.com>
2020-04-27 19:47:57 +0530

    carousel and roadbike image added

--- src/navcomponents/road.js
+++ src/navcomponents/road.js
@@ -1,20 +1,63 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
 import RoadNav from '../component/RoadNav'
+import { Carousel } from 'react-bootstrap'
+import RoadSprint from '../assets/road_images/roadsprint.jpg'
+import RoadParked from '../assets/road_images/roadparked.jpg'
+import BreakAway from '../assets/road_images/breakaway.jpg'
 
 
 
-function Road(){
-    
+function Road() {
+
     return (
         <div>
-        <h2>
-            ROAD
+            <h2>
+                ROAD
         </h2>
-        <RoadNav></RoadNav>
-        
+            <RoadNav></RoadNav>
+
+            <div>
+            <Carousel>
+                <Carousel.Item>
+                    <img style={{innerHeight : "640px", innerWidth : "440px"}}
+                        className="d-block w-100"
+                        src={RoadSprint}
+                        alt="Sprinting"
+                    />
+                    <Carousel.Caption>
+                        <h3>Sonu is Sprinting</h3>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={RoadParked}
+                        alt="Parked bike"
+                    />
+
+                    <Carousel.Caption>
+                        <p>Classical parked bike</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={BreakAway}
+                        alt="Break away"
+                    />
+
+                    <Carousel.Caption>
+                        <p>Bawa is on breakaway.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+
+
+            </div>
+            
         </div>
-       
+
     )
 }
 
